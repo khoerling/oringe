d2212a2270a504b31aba07ce9bc5b722bb3a8c07
mr. keith <keith@hoerling.com>
2017-04-22 16:16:12 -0700

    wip: adds basic Slideshow with Transitions
    - TODO time overlays with slideshow changes
    - TODO use assets from Ben

--- index.ios.js
+++ index.ios.js
@@ -4,62 +4,94 @@
  */
 
 import React, { Component } from 'react'
-import { Animated } from 'react-native'
 import Animation from 'lottie-react-native'
+import {resolveAssetSource, Surface} from 'gl-react-native'
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
+    AppRegistry,
+    StyleSheet,
+    Dimensions,
+    Text,
+    View,
+    Image,
+    Animated,
 } from 'react-native'
 
+import Slideshow from './src/Slideshow'
 import ShaderImage from './src/ShaderImage'
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      progress: new Animated.Value(0),
+const
+  { width, height } = require('Dimensions').get('window'),
+  images = 'wxqlQkh,G2Whuq3,0bUSEBX,giP58XN,iKdXwVm,IvpoR40,zJIxPEo,CKlmtPs,fnMylHI,vGXYiYy,MnOB9Le,YqsZKgc,0BJobQo,Otbz312'
+    .split(',')
+    .map(id => `https://imgur.com/${id}.jpg`)
+
+
 export default class Oringe extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        time: 0.02,
+        frames: 1,
+        progress: new Animated.Value(0),
+      }
     }
-  }
 
-  resetAnimation() {
-    this.state.progress.setValue(0)        // reset
-    Animated.timing(this.state.progress, { // go!
-      toValue: 1,
-      duration: 5000,
-    }).start()
-  }
+    componentDidMount() {
+      this.resetLottieAnimation()
+      this.loopAnimationFrame()
+    }
 
-  componentDidMount() {
-    this.resetAnimation()
-  }
+    componentWillUnmount () {
+      cancelAnimationFrame(this._raf)
+    }
 
-  render() {
-    return (
-      // TODO split this out in src/
-      <View style={styles.background}>
-        <ShaderImage style={styles.backgroundImage} />
-        <View style={styles.container}>
-          <Animation
-            style={styles.animation}
-            source={require('./node_modules/lottie-ios/Lottie-Screenshot/Lottie-Screenshot/PinJump.json')}
-            progress={this.state.progress}
-          />
-          <Text style={styles.header} onPress={ _ => this.resetAnimation() }>
-            Lottie + GPUImage + Fonts
-          </Text>
-          <Text style={styles.ideas} onPress={ _ => this.resetAnimation() }>
-            IDEA |  Use Lottie for a slick, sexy App UI
-          </Text>
-          <Text style={styles.ideas}>
-            TODO |  Build declarative DSL glue'ing GPUImage + Dynamic Font combinations for SlideShows
-          </Text>
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.background}>
+                <Slideshow
+                  styles={styles.backgroundImage}
+                  time={this.state.time}
+                  width={width}
+                  height={height}
+                  images={images.slice(2)}
+                  pauseDuration={3.5}
+                  transitionDuration={2}>
+                    <View style={styles.container}>
+                        <Animation
+                            style={styles.animation}
+                            source={require('./node_modules/lottie-ios/Lottie-Screenshot/Lottie-Screenshot/PinJump.json')}
+                            progress={this.state.progress}
+                        />
+                        <Text style={styles.header} onPress={ _ => this.resetLottieAnimation() }>
+                            Lottie + OpenGL + DynamicFonts
+                        </Text>
+                        <Text style={styles.ideas} onPress={ _ => this.resetLottieAnimation() }>
+                            TODO |  Build declarative DSL glue'ing Lottie, OpenGL + Dynamic Font combinations for timed SlideShows
+                        </Text>
+                    </View>
+                </Slideshow>
+            </View>
+        )
+    }
+
+    resetLottieAnimation() {
+      this.state.progress.setValue(0)        // reset
+      Animated.timing(this.state.progress, { // go!
+        toValue: 1,
+        duration: 5000,
+      }).start()
+    }
+
+    loopAnimationFrame() { // boot glSlideshow frame loop
+      let startTime
+      const loop = t => {
+        this._raf = requestAnimationFrame(loop)
+        if (!startTime) startTime = t
+        const time = (t - startTime) / 1000
+        this.setState({ time: time, frames: this.state.frames+1 })
+      }
+      this._raf = requestAnimationFrame(loop)
+    }
 }
 
 const styles = StyleSheet.create({
@@ -69,46 +101,35 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   animation: {
+    marginLeft: -40,
     width: 300,
     height: 300,
   },
-  image: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
   backgroundImage: {
-    borderWidth: 0,
     position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
   },
   background: {
     flex: 1,
   },
   header: {
-    backgroundColor: 'rgba(255,100,100,.2)',
-    fontFamily: 'Georgia',
+    backgroundColor: 'rgba(255,100,100,.5)',
+    fontFamily: 'Avenir',
+    color: '#fff',
+    fontWeight: 'bold',
     padding: 10,
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-    marginTop: 20,
   },
   ideas: {
     fontFamily: 'Menlo',
-    fontSize: 7,
+    fontSize: 8,
     textAlign: 'center',
-    backgroundColor: '#eee',
+    backgroundColor: '#fff',
     color: '#555',
     padding: 2,
     marginHorizontal: 50,
     marginTop: 10,
   },
-});
+})
 
 AppRegistry.registerComponent('Oringe', () => Oringe)
